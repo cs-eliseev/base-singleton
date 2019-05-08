c3d58c71124e6288d29582e89c797b26be211058
cs-eliseev <a.k.eliseev@gmail.com>
2019-05-08 22:30:01 +0300

    add test data

--- tests-data/ModelSingleton.php
+++ tests-data/ModelSingleton.php
@@ -10,4 +10,22 @@ use cse\base\SingletonTrait;
 class ModelSingleton
 {
     use SingletonTrait;
+
+    protected $param = 0;
+
+    /**
+     * @param int $param
+     */
+    public function setParam(int $param): void
+    {
+        $this->param = $param;
+    }
+
+    /**
+     * @return int
+     */
+    public function getParam(): int
+    {
+        return $this->param;
+    }
 }
