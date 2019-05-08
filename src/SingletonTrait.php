983bc9a4516d39fc0b8d75b08aa4369c53f4da4b
cs-eliseev <a.k.eliseev@gmail.com>
2019-05-08 08:14:52 +0300

    add method get instance

--- src/SingletonTrait.php
+++ src/SingletonTrait.php
@@ -11,5 +11,27 @@ namespace cse\based\traits;
  */
 trait SingletonTrait
 {
+    protected static $instance;
 
+    /**
+     * @param string $instanceKey
+     *
+     * @return SingletonTrait
+     *
+     * @throws \ReflectionException
+     */
+    public static function getInstance(string $instanceKey = ''): self
+    {
+        $instanceKey = empty($instanceKey) ? __CLASS__ : $instanceKey;
+
+        // instance exist
+        if (empty(self::$instance[$instanceKey])) {
+            $reflection = new \ReflectionClass(__CLASS__);
+            $arg = func_get_args();
+            array_shift($arg);
+            self::$instance[$instanceKey] = $reflection->newInstanceArgs($arg);
+        }
+
+        return self::$instance[$instanceKey];
+    }
 }
