d59e026e3f7fc21e8131d03954432a5e8bf50fb1
cs-eliseev <a.k.eliseev@gmail.com>
2019-05-08 08:42:44 +0300

    add singleton exception

---
+++ src/Exceptions/CSESingletonException.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace cse\helpers\Exceptions;
+
+use cse\base\CseExceptions;
+
+/**
+ * Class CSESingletonException
+ *
+ * @package cse\helpers\Exceptions
+ */
+class CSESingletonException extends CseExceptions
+{
+
+}
