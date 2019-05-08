525347a4533023bf155753b7fce7a12c1df9be47
cs-eliseev <a.k.eliseev@gmail.com>
2019-05-08 21:57:57 +0300

    create model singleton

---
+++ test-data/ModelSingleton.php
@@ -0,0 +1,13 @@
+<?php
+
+use cse\based\traits\SingletonTrait;
+
+require_once __DIR__ . '/../autoload.php';
+
+/**
+ * Class ModelSingleton
+ */
+class ModelSingleton
+{
+    use SingletonTrait;
+}
