14ce0e7000e13e076a6bcfd4b09a5dd71f1be896
cs-eliseev <a.k.eliseev@gmail.com>
2019-05-12 17:58:04 +0300

    add unit test get instance

--- tests/TestSingleton.php
+++ tests/TestSingleton.php
@@ -6,5 +6,27 @@ use PHPUnit\Framework\TestCase;
 
 class TestSingleton extends TestCase
 {
+    /**
+     * @runInSeparateProcess
+     */
+    public function testGetInstance(): void
+    {
+        $test1 = 10;
+        $instance = ModelSingleton::getInstance();
+        $instance->setParam($test1);
+        $this->assertEquals($test1, $instance->getParam());
 
+        $test2 = 20;
+        $instance2 = ModelSingleton::getInstance('new');
+        $instance2->setParam($test2);
+        $this->assertEquals($test1, $instance->getParam());
+        $this->assertEquals($test2, $instance2->getParam());
+
+        $test3 = 30;
+        $instance3 = ModelSingleton::getInstance();
+        $instance3->setParam($test3);
+        $this->assertEquals($test3, $instance->getParam());
+        $this->assertEquals($test2, $instance2->getParam());
+        $this->assertEquals($test3, $instance3->getParam());
+    }
 }
