acd6b371a9a38672c5eb63ebd9e3136542f678c6
cs-eliseev <a.k.eliseev@gmail.com>
2019-05-12 17:36:10 +0300

    create examples

---
+++ examples/examples-singleton.php
@@ -0,0 +1,4 @@
+<?php
+
+require_once __DIR__ . '/../autoload.php';
+require_once __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'tests-data' . DIRECTORY_SEPARATOR . 'ModelSingleton.php';
