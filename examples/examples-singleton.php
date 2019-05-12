5064c5632ac924d526deb1cf97f125c97fc8fdbb
cs-eliseev <a.k.eliseev@gmail.com>
2019-05-12 17:36:46 +0300

    add example default instance

--- examples/examples-singleton.php
+++ examples/examples-singleton.php
@@ -2,3 +2,12 @@
 
 require_once __DIR__ . '/../autoload.php';
 require_once __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'tests-data' . DIRECTORY_SEPARATOR . 'ModelSingleton.php';
+
+use cse\base\Exceptions\CSESingletonException;
+
+// Example: get instance
+$label = 'Create default instance: ';
+// Create default instance
+$instance = ModelSingleton::getInstance();
+$instance->setParam(10);
+var_dump($label . $instance->getParam());
