12ba28e71f7704ec6c7063c344bcef168cd0fd46
cs-eliseev <a.k.eliseev@gmail.com>
2019-05-12 17:57:27 +0300

    create test

---
+++ tests/TestSingleton.php
@@ -0,0 +1,10 @@
+<?php
+
+require_once __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'tests-data' . DIRECTORY_SEPARATOR . 'ModelSingleton.php';
+
+use PHPUnit\Framework\TestCase;
+
+class TestSingleton extends TestCase
+{
+
+}
